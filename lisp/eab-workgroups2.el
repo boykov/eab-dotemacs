9e73aa65f17b6f5a5bc40353f408e4c0fbd07017
Evgeny Boykov <artscan@list.ru>
2015-11-16 12:28:11 +1000

    add wg-session-load-on-start

--- lisp/eab-workgroups2.el
+++ lisp/eab-workgroups2.el
@@ -36,6 +36,7 @@
 
 (setq wg-use-default-session-file 't)
 (setq wg-control-frames 'nil)
+(setq wg-session-load-on-start nil)
 (ignore-errors (workgroups-mode 1))
 
 (setq wg-mode-line-decor-divider "")
